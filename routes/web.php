5c261472ccf21969a31162cefd1147071941fafd
Theo Natan <133386307+theonatangoes@users.noreply.github.com>
2025-05-15 18:40:10 -0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -8,3 +8,18 @@ Route::get('/', function () {
 });
 
 Route::resource('postagens', PostagemController::class);
+
+
+Route::resource('postagens', PostagemController::class);
+
+Route::get('/register', [AuthController::class, 'showRegister'])->name('register');
+Route::post('/register', [AuthController::class, 'register']);
+
+Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::get('/logout', [AuthController::class, 'logout'])->name('logout');
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware('auth');
